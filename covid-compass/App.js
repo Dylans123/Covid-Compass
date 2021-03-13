683f45640f44b595d4f90a75292ce6e4cd395d1f
dylans123 <dylanskelly@knights.ucf.edu>
2021-03-13 15:15:05 -0500

    update app

---
+++ covid-compass/App.js
@@ -0,0 +1,30 @@
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { LineChart, Grid } from 'react-native-svg-charts';
+
+export default function App() {
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+
+  return (
+    <View style={styles.container}>
+      <LineChart
+          style={{ height: 200 }}
+          data={data}
+          svg={{ stroke: 'rgb(134, 65, 244)' }}
+          contentInset={{ top: 20, bottom: 20 }}
+      >
+          <Grid />
+      </LineChart>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
