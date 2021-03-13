2680c397db01e26b6e886bb436295aab6a47896a
Enelson Castro <enecastro@knights.ucf.edu>
2021-03-13 18:11:13 -0500

    Added structure to project.

--- covid-compass/App.js
+++ covid-compass/App.js
@@ -1,23 +1,13 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { LineChart, Grid } from 'react-native-svg-charts';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
-
   return (
     <View style={styles.container}>
-      <LineChart
-          style={{ height: 200 }}
-          data={data}
-          svg={{ stroke: 'rgb(134, 65, 244)' }}
-          contentInset={{ top: 20, bottom: 20 }}
-      >
-          <Grid />
-      </LineChart>
+      <Text>Test</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
