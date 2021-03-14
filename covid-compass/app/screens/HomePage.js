44afbb2d043d6f3ab09999baf5d3a13fc808070e
Enelson Castro <enecastro@knights.ucf.edu>
2021-03-14 00:04:34 -0500

    Added map

--- covid-compass/app/screens/HomePage.js
+++ covid-compass/app/screens/HomePage.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const HomePage = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const mapit = (location) => {
+    console.log(location);
+    console.log(location.coords.latitude);
+    console.log(location.coords.longitude);
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}>
+          <Marker
+            key={'1'}
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title={'test1'}
+            description={'test2'}
+          />
+        </MapView>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Button
+    <View style={styles.container}>
+      {location != null && mapit(location)}
+      {/* <Button
         title='Covid Data'
         onPress={() => navigation.navigate('Covid Data Display')}
       />
@@ -15,11 +62,22 @@ const HomePage = ({ navigation }) => {
       <Button
         title='Settings'
         onPress={() => navigation.navigate('Settings Page')}
-      />
+      /> */}
     </View>
   );
 };
 
 export default HomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
