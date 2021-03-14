7d2ca9007011c1fc848d51eaf4f1ac13fb8a4d41
Enelson Castro <enecastro@knights.ucf.edu>
2021-03-14 10:22:00 -0400

    Added data display, but may need to change it to its own page.

--- covid-compass/app/screens/HomePage.js
+++ covid-compass/app/screens/HomePage.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { LineChart, BarChart, Grid } from 'react-native-svg-charts';
 import * as Location from 'expo-location';
 
 const HomePage = ({ navigation }) => {
@@ -9,6 +17,8 @@ const HomePage = ({ navigation }) => {
   const [countyID, setCountyID] = useState(null);
   const [currentData, setCurrentData] = useState();
   const [loadingCurrent, setLoadingCurrent] = useState(true);
+  const [historicData, setHistoricData] = useState();
+  const [loadingHistoric, setLoadingHistoric] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -29,7 +39,13 @@ const HomePage = ({ navigation }) => {
           lat +
           '&lon=' +
           lon +
-          '&format=json'
+          '&format=json',
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
       )
         .then(async (res) => await res.json())
         .then((res) => {
@@ -45,12 +61,18 @@ const HomePage = ({ navigation }) => {
     fetch(
       'https://api.covidactnow.org/v2/county/' +
         countyID +
-        '.json?apiKey=81fd4c1a6dae45a5a3f3e07983754248'
+        '.json?apiKey=81fd4c1a6dae45a5a3f3e07983754248',
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
     )
       .then(async (res) => await res.json())
       .then((res) => {
         // console.log('current');
-        console.log(res);
+        // console.log(res);
         const {
           cases,
           deaths,
@@ -68,9 +90,54 @@ const HomePage = ({ navigation }) => {
       .catch((error) => {
         console.log(error);
       });
+
+    fetch(
+      'https://api.covidactnow.org/v2/county/' +
+        countyID +
+        '.timeseries.json?apiKey=81fd4c1a6dae45a5a3f3e07983754248'
+    )
+      .then(async (res) => await res.json())
+      .then((res) => {
+        // console.log('historic');
+        // console.log(res);
+        const totalCases = res.actualsTimeseries.map((dataPoint) => {
+          if (dataPoint.cases) {
+            return dataPoint.cases;
+          }
+        });
+        const newDeaths = res.actualsTimeseries.map((dataPoint) => {
+          if (dataPoint.newDeaths) {
+            return dataPoint.newDeaths;
+          }
+        });
+        //console.log('newDeaths');
+        //console.log(newDeaths);
+        const historic = {
+          totalCases,
+          newDeaths,
+        };
+        // console.log(historic)
+        setHistoricData(historic);
+        setLoadingHistoric(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [countyID]);
 
-  const mapit = (location, countyID, currentData) => {
+  const mapit = (location, countyID, currentData, historicData) => {
+    const { county, liveData } =
+      currentData != null
+        ? currentData
+        : { county: 'Loading county...', liveData: 'Loading live data...' };
+    const { totalCases, newDeaths } =
+      historicData != null
+        ? historicData
+        : {
+            totalCases: 'Loading total cases...',
+            newDeaths: 'Loading new deaths...',
+          };
+
     return (
       <View style={styles.container}>
         <MapView
@@ -88,15 +155,42 @@ const HomePage = ({ navigation }) => {
               longitude: location.coords.longitude,
             }}
             title={countyID != null ? countyID : 'test title'}
-            description={
-              currentData != null ? currentData.county : 'test description'
-            }>
-            <View style={{ backgroundColor: 'gray', padding: 10 }}>
-              <Text>
-                {currentData != null
-                  ? JSON.stringify(currentData)
-                  : 'test live data'}
-              </Text>
+            description={county}>
+            <View style={{ backgroundColor: '#CDCDCD', padding: '10%' }}>
+              <Text>Information for {county}</Text>
+              <Text>Live Data</Text>
+              <ScrollView>
+                {liveData != 'Loading live data...' && (
+                  <BarChart
+                    style={{ height: 200, width: 250 }}
+                    data={liveData}
+                    svg={{ fill: 'rgb(134, 65, 244)' }}
+                    contentInset={{ top: 30, bottom: 30 }}>
+                    <Grid />
+                  </BarChart>
+                )}
+
+                <Text>Total Cases</Text>
+                {totalCases != 'Loading total cases...' && (
+                  <LineChart
+                    style={{ height: 200, width: 250 }}
+                    data={totalCases}
+                    svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    contentInset={{ top: 20, bottom: 20 }}>
+                    <Grid />
+                  </LineChart>
+                )}
+                <Text>New Deaths</Text>
+                {newDeaths != 'Loading new deaths...' && (
+                  <LineChart
+                    style={{ height: 200, width: 250 }}
+                    data={newDeaths}
+                    svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    contentInset={{ top: 20, bottom: 20 }}>
+                    <Grid />
+                  </LineChart>
+                )}
+              </ScrollView>
             </View>
           </Marker>
         </MapView>
@@ -106,10 +200,10 @@ const HomePage = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      {console.log(location)}
+      {/* {console.log(location)}
       {console.log(countyID)}
-      {console.log(currentData)}
-      {location != null && mapit(location, countyID, currentData)}
+      {console.log(currentData)} */}
+      {location != null && mapit(location, countyID, currentData, historicData)}
     </View>
   );
 };
