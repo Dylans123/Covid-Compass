0bff754a2415d06f829d4e8ab4576a8b463e7239
unknown <harold.padilla64@gmail.com>
2021-03-14 12:45:40 -0400

    Re add button from previous commit, not new code

--- covid-compass/app/screens/StartPage.js
+++ covid-compass/app/screens/StartPage.js
@@ -12,7 +12,16 @@ const StartPage = ({ navigation }) => {
   return (
     <SafeAreaView>
       <StatusBar backgroundColor='#000' barStyle='light-content' />
-      <Button title='Start' onPress={() => navigation.navigate('Home Page')} />
+      <View
+      style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100%', width:'100%'}}
+    >
+      <Text
+        style={{textAlign:'center', fontSize:40, marginBottom:150}}
+      >
+        COVID Compass
+      </Text>
+      <Button title='Enter COVID Compass 🧭' onPress={() => navigation.navigate('Home Page')} />
+      </View>
     </SafeAreaView>
   );
 };
